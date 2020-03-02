f7765837aaaa8f6e513ae4bbf5376fa4cd36118f
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-03-02 17:06:10 +0100

    Modifica3

--- services/temperature/incoming_webhooks/cerca/source.js
+++ services/temperature/incoming_webhooks/cerca/source.js
@@ -20,7 +20,7 @@ exports = function(payload, response) {
     // when the "Respond with Result" setting is set.
     // return  "Hello World!";
     var collection = context.services.get("mongodb-atlas").db("Meteo").collection("Citta");
-    var t = collection.findOne({nome: "Trento"}).then(
+    var t = collection.findOne({nome: bbb}).then(
        (doc)=>{return doc.temperatura.toString()}
     );
     return t;
